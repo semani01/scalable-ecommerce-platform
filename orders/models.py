7c2d0a2100ce0f26f1a3017ad460cdee6c761827
Sai Srikar Emani <esrikar01@gmail.com>
2025-02-03 21:41:05 -0700

    Add models for Cart and Orders

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings  # Use AUTH_USER_MODEL
+from products.models import Product
 
-# Create your models here.
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Order({self.id}) - {self.user.username}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_items')
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in Order {self.order.id}"
